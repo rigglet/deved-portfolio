411756851c471fb63069a087c4f3abae8e2d3294
rigglet <39748939+rigglet@users.noreply.github.com>
2020-12-17 22:19:59 +0000

    4. FAQ Section

---
+++ src/components/FAQSection.js
@@ -0,0 +1,54 @@
+import react from "react";
+import home1 from "../img/home1.png";
+
+const FAQSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde,
+            quisquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily schedule?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde,
+            quisquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment options?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde,
+            quisquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde,
+            quisquam.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import react from "react";
 //import page components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FAQSection from "../components/FAQSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FAQSection />
     </>
   );
 };
