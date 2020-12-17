c32e93c67b2f9f86f4a7f706ac9266baf479ec0f
rigglet <39748939+rigglet@users.noreply.github.com>
2020-12-17 21:19:07 +0000

    2. About Us

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import react from "react";
+//import page components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
