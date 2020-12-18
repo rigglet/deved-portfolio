fa1cabc02b80287e1ce9ef9f109386fd5ea2e44b
rigglet <39748939+rigglet@users.noreply.github.com>
2020-12-18 13:29:42 +0000

    5. Styled About Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 //import pages
 import AboutUs from "./pages/AboutUs";
+//global style
+import GlobalStyle from "./components/GlobalStyle";
+import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Film</h1>
       <AboutUs />
     </div>
