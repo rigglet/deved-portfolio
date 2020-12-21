59cce3258317cd94086dee38b0a4d4019832385a
rigglet <39748939+rigglet@users.noreply.github.com>
2020-12-21 13:12:46 +0000

    12. About animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import home1 from "../img/home1.png";
 import { Layout, Description, Hide, Image } from "../styles/styles";
 //Framer motion
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -10,26 +11,26 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact us here</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us here</motion.button>
       </Description>
-      <Image>
+      <motion.Image variants={photoAnimation}>
         <img src={home1} alt="Guy with a camera" />
-      </Image>
+      </motion.Image>
     </Layout>
   );
 };
