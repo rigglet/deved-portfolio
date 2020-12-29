84d6a431f65da92f351a865845dbc40259446849
rigglet <39748939+rigglet@users.noreply.github.com>
2020-12-29 15:59:55 +0000

    20. Responsive pages

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -44,6 +44,10 @@ const ContactStyled = styled(motion.div)`
   padding: 5rem 10rem;
   color: #353535;
   min-height: 90vh;
+  @media (max-width: 1300px) {
+    padding: 2rem;
+    font-size: 1rem;
+  }
 `;
 const Title = styled.div`
   color: black;
